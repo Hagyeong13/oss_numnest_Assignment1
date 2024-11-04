dd9575fa6fbdfe618800e5a5089c8b65ff2a824e
hagyeong <hagyeong13@naver.com>
2024-11-04 23:41:45 +0900

    prime lib test

--- lib.test.js
+++ lib.test.js
@@ -8,3 +8,15 @@ test("avg([3, 5, 7]) should be 5", () => {
 test("avg[-5, 5] should be 0", () => {
     expect(lib.avg([-5, 5])).toBe(0);
 });
+
+test("prime(3) should be true", () => {
+    expect(lib.prime(3)).toBe(true);
+});
+
+test("prime(-1) should be false", () => {
+    expect(lib.prime(-1)).toBe(false);
+});
+
+test("prime(15) should be false", () => {
+    expect(lib.prime(15)).toBe(false);
+});
